4b3af690fa27e007614246a4d8befe49abae9bed
Maral <mbaizhuminova@gmail.com>
2021-12-04 00:05:52 +0600

    minor changes

--- src/main/java/com/example/postgresql/controllers/MainController.java
+++ src/main/java/com/example/postgresql/controllers/MainController.java
@@ -4,11 +4,9 @@ import com.example.postgresql.models.Form;
 import com.example.postgresql.models.User;
 import com.example.postgresql.services.UserAdvancedService;
 import com.example.postgresql.services.UserService;
-import com.example.postgresql.services.ValidationService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +21,11 @@ public class MainController {
 
     private final UserService userService;
     private final UserAdvancedService userAdvancedService;
-    private final ValidationService validationService;
 
-    public MainController(UserService userService, UserAdvancedService userAdvancedService, ValidationService validationService) {
+    public MainController(UserService userService, UserAdvancedService userAdvancedService) {
 
         this.userService = userService;
         this.userAdvancedService = userAdvancedService;
-        this.validationService = validationService;
     }
 
     @GetMapping("/")
@@ -105,12 +101,6 @@ public class MainController {
     @PostMapping("/adduser")
     public String addUser(@Valid User user, BindingResult result, Model model) {
 
-        String err = validationService.validateUser(user);
-
-        if (!err.isEmpty()) {
-            ObjectError error = new ObjectError("globalError", err);
-            result.addError(error);
-        }
         if (result.hasErrors()) {
             return "add-user";
         }
@@ -130,13 +120,6 @@ public class MainController {
     @PostMapping("/update/{email}")
     public String updateUser(@PathVariable("email") String email, @Valid User user,
                              BindingResult result, Model model) {
-
-        String err = validationService.validateUser(user);
-
-        if (!err.isEmpty()) {
-            ObjectError error = new ObjectError("globalError", err);
-            result.addError(error);
-        }
 
         if (result.hasErrors()) {
             user.setEmail(email);
